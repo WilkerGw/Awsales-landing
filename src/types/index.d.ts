aca4cd55b38fa35c863447e570cf8ed3ececb6a3
Wilker Martins <dev.wilker@gmail.com>
2025-07-28 13:20:38 -0300

    thirt comm

--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -1,8 +1,8 @@
-// src/types/index.d.ts (ou src/types/features.d.ts)
+// src/types/index.d.ts
+
+import React from 'react';
 
 // Definindo o tipo para os ícones SVG, que são elementos JSX.
-// React.ReactNode é um tipo abrangente que inclui elementos JSX, strings, números, etc.
-import UserHighlight from '../components/UserHighlight';
 type FeatureIcon = React.ReactNode;
 
 export interface Feature {
@@ -12,32 +12,4 @@ export interface Feature {
   title: string;
   subtitle: string;
   description: string;
-}
-
-
-// pages/index.jsx ou pages/index.js
-
-export default function HomePage() {
-  const bettinaData = {
-    imageSrc: 'http://googleusercontent.com/file_content/0', // URL da imagem da Bettina
-    imageAlt: 'Bettina Rudolph, uma das maiores referências no meio digital do Brasil',
-    category: 'QUEM USA',
-    name: 'Bettina Rudolph',
-    description:
-      'Considerada uma das maiores referências no meio digital do Brasil, utilizou nossos agentes de IA em seu lançamento através do WhatsApp.',
-    stats: [
-      'Aumento de 63% de comparecimento nas lives.',
-      '90% de entrada nos grupos do WhatsApp.',
-    ],
-    buttonText: 'Começar agora',
-    followers: '1.466.398',
-  };
-
-  return (
-    <div>
-      {/* Você pode ter outros elementos aqui */}
-      <UserHighlight {...bettinaData} />
-      {/* Você pode ter outros elementos aqui */}
-    </div>
-  );
 }
